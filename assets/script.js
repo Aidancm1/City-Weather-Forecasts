ded22e47b2a0ee6ba74fe31d75526a242a1d79bd
Aidancm1 <128535723+Aidancm1@users.noreply.github.com>
2023-06-27 19:20:56 -0400

    got api fetches to work

--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,22 @@ const forecastContainerEl = document.querySelector("#forecase-container");
 const citySearchResult = document.querySelector("#city-search-result");
 
 function coordinateApi (lat, lon) {
-    let weatherApi = "http://api.openweathermap.org/geo/1.0/reverse?lat="+lat+"&lon="+lon+"&limit={limit}&appid=aeccee0135d3897dac48165b89fd0c1e&units=imperial";
-    fetch(coordinateApi)
+    console.log(lat, lon);
+    let weatherApi = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&cnt=5&appid=aeccee0135d3897dac48165b89fd0c1e&units=imperial";
+    
+    fetch(weatherApi)
     .then(function(response) {
         return response.json();
     })
     .then(function (data) {
 
         console.log(data);
-        coordinateApi();
     })
 }
 
+
     function cityApi(cityName){
-        let cityApi = "http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=5&appid=aeccee0135d3897dac48165b89fd0c1e";
+        let cityApi = "https://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=5&appid=aeccee0135d3897dac48165b89fd0c1e";
         fetch(cityApi)
         .then(function(response) {
             return response.json();
@@ -27,14 +29,16 @@ function coordinateApi (lat, lon) {
             let lat = data[0].lat;
             let lon = data[0].lon;
             console.log(lat,lon);
+            coordinateApi(lat, lon);
         })
   }
+
 
   let formSearchHandler = function (event) {
 
     event.preventDefault();
 
-    let previousSearch = citySearchResult.value.trim();
+    let previousSearch = cityInputEl.value.trim();
 
     if(previousSearch) {
         cityApi(previousSearch);
@@ -43,3 +47,6 @@ function coordinateApi (lat, lon) {
         alert("Please search for a City");
     }
   };
+
+
+cityFormEl.addEventListener("submit", formSearchHandler)
